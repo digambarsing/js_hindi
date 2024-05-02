cf27c2842654afc51c59a1b9a5841111ba525485
Digambar singh <138235609+digambarsing@users.noreply.github.com>
2024-05-02 21:54:53 +0530

    Add files via upload

---
+++ domanipulation.js
@@ -0,0 +1,14 @@
+// let a=document.getElementById("myid").style.backgroundColor="blue";
+// a.innerText="its a dom manipulation."
+// console.log(a.innerText);
+
+
+// let b=document.querySelectorAll(".cls");
+// console.log(b.innerText);
+// b.innerText="changew everyone";
+
+
+
+let a=document.getElementById("heading");
+a.append("from college");
+console.log(a.innerText);
