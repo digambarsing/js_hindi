66f4df800a088223b5bb2decc160e7888e5140c4
Digambar singh <138235609+digambarsing@users.noreply.github.com>
2024-05-03 11:27:02 +0530

    Add files via upload

---
+++ conversion.js
@@ -0,0 +1,31 @@
+let n=33 //33
+let q="33" //33
+let w="33a" //nan
+let e=true//1
+let r=null //0
+let t; // nan
+
+let num=Number(n)
+let num1=Number(q)
+let num2=Number(w)
+let num3=Number(e)
+let num4=Number(r)
+let num5=Number(t)
+//console.table( [num,num1,num2,num3,num4,num5])
+
+
+let a="sa" //sa
+let s=33 //33
+let d=true //true
+let f=null //null
+let g //.undefined
+
+let st=String(a)
+let st1=String(s)
+let st2=String(d)
+let st3=String(f)
+let st4=String(g)
+console.table( [st,st1,st2,st3,st4])
+
+
+
---
+++ datatype.js
@@ -0,0 +1,14 @@
+"use strict"; // it is used to tell code based on modern js.
+
+let fname="ved"; // string
+let age=89; // number
+let isLogged=true; // boolean
+let temp=null; //object(null)
+let address; // undefined
+let stockItem=BigInt(45); // bigint
+
+console.table([fname,address,age,isLogged,temp,stockItem])
+
+console.log(typeof(fname));
+console.log(typeof stockItem);
+console.log(typeof(temp));
